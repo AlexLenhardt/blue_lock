fe18e33e8847e9be49864aa1136151c7000f726c
AlexLenhardt <alex.lenhardt@outlook.com>
2023-06-29 10:11:43 -0300

    ...

---
+++ src/main/kotlin/com/backend/blue_lock/BlueLockApplication.kt
@@ -0,0 +1,11 @@
+package com.backend.blue_lock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlueLockApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlueLockApplication>(*args)
+}
---
+++ src/test/kotlin/com/backend/blue_lock/BlueLockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backend.blue_lock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlueLockApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
